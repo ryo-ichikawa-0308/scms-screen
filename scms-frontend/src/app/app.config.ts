05ed64abe575b652faf471b67f09073ac220f4ca
Ryo ICHIKAWA <ryo.ichikawa.0308@gmail.com>
2025-11-29 13:24:27 +0000

    Karma設定

--- scms-frontend/src/app/app.config.ts
+++ scms-frontend/src/app/app.config.ts
@@ -18,6 +18,9 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(withInterceptors([authInterceptor])), provideServiceWorker('ngsw-worker.js', {
             enabled: !isDevMode(),
             registrationStrategy: 'registerWhenStable:30000'
+          }), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
           }),
     //provideAnimations()
   ],
