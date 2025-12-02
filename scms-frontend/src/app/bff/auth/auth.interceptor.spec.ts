4f2d6d3714078abb0e7ba39a0b9ed537d0f71d74
Ryo ICHIKAWA <ryo.ichikawa.0308@gmail.com>
2025-12-02 14:26:45 +0000

    テストコードのリファクタリング

--- scms-frontend/src/app/bff/auth/auth.interceptor.spec.ts
+++ scms-frontend/src/app/bff/auth/auth.interceptor.spec.ts
@@ -46,77 +46,75 @@ describe('AuthInterceptor', () => {
 
     spyOn(authService, 'getAccessToken').and.returnValue(MOCK_TOKEN);
     spyOn(authService, 'logout');
+    spyOn(authService, 'getIsRefreshing').and.returnValue(false);
+    spyOn(authService, 'isAccessTokenExpired').and.returnValue(false); 
   });
 
   afterEach(() => {
     httpMock.verify();
   });
 
-  describe('authInterceptor', () => {
-    describe('正常系', () => {
-      it('有効なトークンをヘッダーに付与し、APIリクエストが成功すること', (done) => {
-        authService.getAccessToken = jasmine.createSpy().and.returnValue(MOCK_TOKEN);
-        httpClient.get(TEST_URL).subscribe((res) => {
-          expect(res).toBeTruthy();
-          done();
-        });
-        const req = httpMock.expectOne(TEST_URL);
-        expect(req.request.headers.get('Authorization')).toBe(`Bearer ${MOCK_TOKEN}`);
-        req.flush({ data: 'ok' });
-
-        describe('401エラーからのリフレッシュ', () => {
-          it('401エラーを受け取るとリフレッシュを実行し、元のリクエストを新しいトークンで再試行する', (done) => {
-            authService.getAccessToken = jasmine.createSpy().and.returnValue(MOCK_TOKEN);
-            spyOn(authService, 'isAccessTokenExpired').and.returnValue(true);
-            const refreshTokenSpy = spyOn(authService, 'refreshToken').and.returnValue(
-              of(REFRESH_TOKEN_RESPONSE),
-            );
-
-            httpClient.get(TEST_URL).subscribe(
-              (res) => {
-                expect(res).toEqual({ data: 'retry_ok' }); // 再試行リクエストの応答
-                expect(refreshTokenSpy).toHaveBeenCalledTimes(1); // リフレッシュが1回呼び出されたこと
-                done();
-              },
-              () => fail('リクエストが失敗しました'),
-            );
-
-            // 1. 最初のHTTPリクエスト (401を返す)
-            let initialReq = httpMock.expectOne(TEST_URL);
-            initialReq.flush(null, { status: 401, statusText: 'Unauthorized' });
-
-            // 2. 再試行されたHTTPリクエスト (新しいトークンを検証)
-            let retryReq = httpMock.expectOne(TEST_URL);
-            expect(retryReq.request.headers.get('Authorization')).toBe(
-              `Bearer ${REFRESH_TOKEN_RESPONSE.token.accessToken}`,
-            );
-            retryReq.flush({ data: 'retry_ok' }); // 成功レスポンス
-          });
-        });
-      });
+  it('有効なトークンをヘッダーに付与し、APIリクエストが成功すること', (done) => {
+    httpClient.get(TEST_URL).subscribe((res) => {
+      expect(res).toBeTruthy();
+      done();
     });
-    describe('異常系', () => {
-      it('401エラーを受け取り、リフレッシュに失敗するとログアウト処理を行う', (done) => {
-        authService.getAccessToken = jasmine.createSpy().and.returnValue(MOCK_TOKEN);
-        spyOn(authService, 'isAccessTokenExpired').and.returnValue(true);
-        spyOn(authService, 'refreshToken').and.returnValue(
-          throwError(() => new HttpErrorResponse({ status: 400 })),
-        );
-
-        httpClient.get(TEST_URL).subscribe({
-          next: () => fail('リクエストは失敗するべきです'),
-          error: (error) => {
-            // 検証: ログアウトと遷移
-            expect(authService.logout).toHaveBeenCalled();
-            expect(router.navigate).toHaveBeenCalledWith(['/login']);
-            done();
-          },
-        });
-        let initialReq = httpMock.expectOne(TEST_URL);
-        initialReq.flush(null, { status: 401, statusText: 'Unauthorized' });
-      });
-    });
+    
+    const req = httpMock.expectOne(TEST_URL);
+    expect(req.request.headers.get('Authorization')).toBe(`Bearer ${MOCK_TOKEN}`);
+    req.flush({ data: 'ok' });
   });
 
+  describe('401エラーからのリフレッシュ', () => {
+    it('401エラーを受け取るとリフレッシュを実行し、元のリクエストを新しいトークンで再試行する', (done) => {
+      (authService.isAccessTokenExpired as jasmine.Spy).and.returnValue(true);
+      const refreshTokenSpy = spyOn(authService, 'refreshToken').and.returnValue(
+        of(REFRESH_TOKEN_RESPONSE),
+      );
 
+      httpClient.get(TEST_URL).subscribe(
+        (res) => {
+          expect(res).toEqual({ data: 'retry_ok' });
+          expect(refreshTokenSpy).toHaveBeenCalledTimes(1);
+          done();
+        },
+        () => fail('リクエストが失敗しました'),
+      );
+
+      // 1. 最初のHTTPリクエスト (401を返す)
+      let initialReq = httpMock.expectOne(TEST_URL);
+      expect(initialReq.request.headers.get('Authorization')).toBe(`Bearer ${MOCK_TOKEN}`);
+      initialReq.flush(null, { status: 401, statusText: 'Unauthorized' });
+
+      // 2. リフレッシュ成功後の再試行されたHTTPリクエスト (新しいトークンを検証)
+      let retryReq = httpMock.expectOne(TEST_URL);
+      expect(retryReq.request.headers.get('Authorization')).toBe(
+        `Bearer ${REFRESH_TOKEN_RESPONSE.token.accessToken}`,
+      );
+      retryReq.flush({ data: 'retry_ok' });
+    });
+
+    it('401エラーを受け取り、リフレッシュに失敗するとログアウト処理を行う', (done) => {
+      (authService.isAccessTokenExpired as jasmine.Spy).and.returnValue(true);
+      const refreshTokenSpy = spyOn(authService, 'refreshToken').and.returnValue(
+        throwError(() => new HttpErrorResponse({ status: 400 })),
+      );
+
+      httpClient.get(TEST_URL).subscribe({
+        next: () => fail('リクエストは失敗するべきです'),
+        error: (error) => {
+          expect(refreshTokenSpy).toHaveBeenCalledTimes(1);
+          expect(authService.logout).toHaveBeenCalled();
+          expect(router.navigate).toHaveBeenCalledWith(['/login']);
+          done();
+        },
+      });
+
+      // 1. 最初のHTTPリクエスト (401を返す)
+      let initialReq = httpMock.expectOne(TEST_URL);
+      initialReq.flush(null, { status: 401, statusText: 'Unauthorized' });
+
+      // 2. リフレッシュAPIが失敗したため、リトライは発生せず、Observableチェーンがエラーで終了する
+    });
+  });
 });
